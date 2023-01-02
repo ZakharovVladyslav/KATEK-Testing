471817334c3dfacd350fe447e2dc307a42c15090
ZakharovVladyslav <vladislavozakharov@gmail.com>
2023-01-02 15:17:23 +0200

    Initial easy variant of side bar

--- V/SideBar/sidebar.js
+++ V/SideBar/sidebar.js
@@ -1,3 +1,19 @@
-const h1 = document.querySelector('#h1')
+const openSideBarCheckbox = document.getElementById('open-side-bar')
+const openSideBarLabel = document.getElementById('open-side-bar-label')
+const sidePanel = document.getElementById('side-section')
 
-h1.innerHTML = "Hello"
+openSideBarCheckbox.onchange = (e) => {
+    if (openSideBarCheckbox.checked) {
+        sidePanel.style.width = "250px"
+        sidePanel.style.borderRight = "1px solid #00ffff"
+        openSideBarLabel.style.marginLeft = "250px"
+        openSideBarLabel.style.transform = 'rotate(90deg)'
+        openSideBarLabel.style.transition = '0.5s ease-in-out'
+    }
+    else {
+        sidePanel.style.width = "0px"
+        setTimeout(() => sidePanel.style.borderRight = "0px", 500)
+        openSideBarLabel.style.transform = 'rotate(-45deg)'
+        openSideBarLabel.style.marginLeft = "0px"
+    }
+}
