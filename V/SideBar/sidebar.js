74f771e46c661f28f68afc8902a800bdac907899
ZakharovVladyslav <vladislavozakharov@gmail.com>
2023-01-02 15:52:43 +0200

    Finished version of sideBar

--- V/SideBar/sidebar.js
+++ V/SideBar/sidebar.js
@@ -4,16 +4,16 @@ const sidePanel = document.getElementById('side-section')
 
 openSideBarCheckbox.onchange = (e) => {
     if (openSideBarCheckbox.checked) {
-        sidePanel.style.width = "250px"
+        sidePanel.style.width = "550px"
         sidePanel.style.borderRight = "1px solid #00ffff"
-        openSideBarLabel.style.marginLeft = "250px"
-        openSideBarLabel.style.transform = 'rotate(90deg)'
+        openSideBarLabel.style.marginLeft = "550px"
+        openSideBarLabel.style.transform = 'rotate(360deg)'
         openSideBarLabel.style.transition = '0.5s ease-in-out'
     }
     else {
         sidePanel.style.width = "0px"
         setTimeout(() => sidePanel.style.borderRight = "0px", 500)
-        openSideBarLabel.style.transform = 'rotate(-45deg)'
+        openSideBarLabel.style.transform = 'rotate(-360deg)'
         openSideBarLabel.style.marginLeft = "0px"
     }
 }
